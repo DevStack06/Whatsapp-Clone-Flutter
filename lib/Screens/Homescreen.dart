1d2987ebbda1bac84fc728b4a58d770e1666597b
Balram Rathore <balram0698@gmail.com>
2020-12-18 11:43:05 +0530

    chat app work started

---
+++ lib/Screens/Homescreen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  Homescreen({Key key}) : super(key: key);
+
+  @override
+  _HomescreenState createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen>
+    with SingleTickerProviderStateMixin {
+  TabController _controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = TabController(length: 4, vsync: this, initialIndex: 1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Whatsapp Clone"),
+        actions: [
+          IconButton(icon: Icon(Icons.search), onPressed: () {}),
+          IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
+        ],
+        bottom: TabBar(
+          controller: _controller,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              text: "CHATS",
+            ),
+            Tab(
+              text: "STATUS",
+            ),
+            Tab(
+              text: "CALLS",
+            )
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: [
+          Text("camera"),
+          Text("Chats"),
+          Text("Status"),
+          Text("Calss"),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:chatapp/Screens/Homescreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Color(0xFF075E54), accentColor: Color(0xFF128C7E)),
+      home: Homescreen(),
+    );
+  }
+}
