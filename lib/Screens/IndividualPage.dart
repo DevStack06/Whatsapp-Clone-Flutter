82e075a98dd906bd4cfc9e621dbd6c08fa46418d
Balram Rathore <balram0698@gmail.com>
2021-01-07 21:43:16 +0530

    individualpage work started

--- lib/Screens/IndividualPage.dart
+++ lib/Screens/IndividualPage.dart
@@ -11,7 +11,56 @@ class _IndividualPageState extends State<IndividualPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: AppBar(
+          titleSpacing: 0,
+          leadingWidth: 70,
+          leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.arrow_back,
+                  size: 24,
+                ),
+                CircleAvatar(
+                  radius: 18,
+                  backgroundColor: Colors.blueGrey,
+                ),
+              ],
+            ),
+          ),
+          title: InkWell(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.all(3),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Dev Stack",
+                    style: TextStyle(
+                      fontSize: 19,
+                      // fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    "last seen today at 12:05",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
